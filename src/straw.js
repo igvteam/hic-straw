82a6b889e225b7105d2d03236a13f6bd8d18ec28
Jim Robinson <jrobinso@broadinstitute.org>
2019-03-23 21:02:30 -0700

    Add low-level getBlocks function.  Needed by juicebox

--- src/straw.js
+++ src/straw.js
@@ -14,75 +14,85 @@ class Straw {
         return await this.hicFile.getMetaData()
     }
 
+    async getBlocks(region1, region2, units, binsize) {
+
+        await this.hicFile.init()
+
+        const chr1 = this.hicFile.getFileChrName(region1.chr)
+        const chr2 = this.hicFile.getFileChrName(region2.chr)
+        const idx1 = this.hicFile.chromosomeIndexMap[chr1]
+        const idx2 = this.hicFile.chromosomeIndexMap[chr2]
+        const x1 = (region1.start === undefined) ? undefined : region1.start / binsize
+        const x2 = (region1.end === undefined) ? undefined : region1.end / binsize
+        const y1 = (region2.start === undefined) ? undefined : region2.start / binsize
+        const y2 = (region2.end === undefined) ? undefined : region2.end / binsize
+
+        const matrix = await this.hicFile.readMatrix(idx1, idx2)
+
+        // Find the requested resolution
+        const z = undefined === binsize ? 0 : this.hicFile.getZoomIndexForBinSize(binsize, units);
+        if (z === -1) {
+            throw new Error("Invalid bin size");
+        }
+
+        const zd = matrix.bpZoomData[z]
+        const blockBinCount = zd.blockBinCount   // Dimension in bins of a block (width = height = blockBinCount)
+        const col1 = x1 === undefined ? 0 : Math.floor(x1 / blockBinCount)
+        const col2 = x1 === undefined ? zd.blockColumnCount : Math.floor(x2 / blockBinCount)
+        const row1 = y1 === undefined ? 0 : Math.floor(y1 / blockBinCount)
+        const row2 = y2 === undefined ? zd.blockColumnCount : Math.floor(y2 / blockBinCount)
+
+        const promises = [];
+        const sameChr = idx1 === idx2;
+        for (let row = row1; row <= row2; row++) {
+            for (let column = col1; column <= col2; column++) {
+                let blockNumber
+                if (sameChr && row < column) {
+                    blockNumber = column * zd.blockColumnCount + row;
+                }
+                else {
+                    blockNumber = row * zd.blockColumnCount + column;
+                }
+                promises.push(this.hicFile.readBlock(blockNumber, zd))
+            }
+        }
+
+        return Promise.all(promises)
+    }
 
     //straw <NONE/VC/VC_SQRT/KR> <ile> <chr1>[:x1:x2] <chr2>[:y1:y2] <BP/FRAG> <binsize>
     async getContactRecords(normalization, region1, region2, units, binsize) {
 
-        try {
-            await this.hicFile.init()
+        const blocks = await this.getBlocks(region1, region2, units, binsize)
 
-            const chr1 = this.hicFile.getFileChrName(region1.chr)
-            const chr2 = this.hicFile.getFileChrName(region2.chr)
-            const x1 = (region1.start === undefined) ? undefined : region1.start / binsize
-            const x2 = (region1.end === undefined) ? undefined : region1.end / binsize
-            const y1 = (region2.start === undefined) ? undefined : region2.start / binsize
-            const y2 = (region2.end === undefined) ? undefined : region2.end / binsize
+        const chr1 = this.hicFile.getFileChrName(region1.chr)
+        const chr2 = this.hicFile.getFileChrName(region2.chr)
+        const x1 = (region1.start === undefined) ? undefined : region1.start / binsize
+        const x2 = (region1.end === undefined) ? undefined : region1.end / binsize
+        const y1 = (region2.start === undefined) ? undefined : region2.start / binsize
+        const y2 = (region2.end === undefined) ? undefined : region2.end / binsize
 
-            const matrix = await this.hicFile.readMatrix(chr1, chr2)
-
-            // Find the requested resolution
-            const z = undefined === binsize ? 0 : this.hicFile.getZoomIndexForBinSize(binsize, units);
-            if (z === -1) {
-                throw new Error("Invalid bin size");
+        let normVector1
+        let normVector2
+        const isNorm = normalization && normalization !== "NONE"
+        if (isNorm) {
+            normVector1 = await this.hicFile.getNormalizationVector(normalization, chr1, units, binsize)
+            if (chr1 === chr2) {
+                normVector2 = normVector1
+            } else {
+                normVector2 = await this.hicFile.getNormalizationVector(normalization, chr2, units, binsize)
             }
-
-            const zd = matrix.bpZoomData[z]
-            const blockBinCount = zd.blockBinCount   // Dimension in bins of a block (width = height = blockBinCount)
-            const col1 = x1 === undefined ? 0 : Math.floor(x1 / blockBinCount)
-            const col2 = x1 === undefined ? zd.blockColumnCount : Math.floor(x2 / blockBinCount)
-            const row1 = y1 === undefined ? 0 : Math.floor(y1 / blockBinCount)
-            const row2 = y2 === undefined ? zd.blockColumnCount : Math.floor(y2 / blockBinCount)
-
-            const promises = [];
-            const sameChr = chr1 === chr2;
-            for (let row = row1; row <= row2; row++) {
-                for (let column = col1; column <= col2; column++) {
-                    let blockNumber
-                    if (sameChr && row < column) {
-                        blockNumber = column * zd.blockColumnCount + row;
-                    }
-                    else {
-                        blockNumber = row * zd.blockColumnCount + column;
-                    }
-                    promises.push(this.hicFile.readBlock(blockNumber, zd))
-                }
-            }
-
-            const blocks = await Promise.all(promises);
-
-            let normVector1
-            let normVector2
-            const isNorm = normalization && normalization !== "NONE"
-            if (isNorm) {
-                normVector1 = await this.hicFile.getNormalizationVector(normalization, chr1, units, binsize)
-                if (chr1 === chr2) {
-                    normVector2 = normVector1
-                } else {
-                    normVector2 = await this.hicFile.getNormalizationVector(normalization, chr2, units, binsize)
-                }
-            }
+        }
 
 
-            const contactRecords = [];
-            blocks.forEach(function (block) {
-                if (!block) { // This is most likely caused by a base pair range outside the chromosome
-                    return;
-                }
-                block.records.forEach(function (rec) {
+        const contactRecords = [];
+        for (let block of blocks) {
 
-                    // TODO -- transpose?
+            if (block) { // This is most likely caused by a base pair range outside the chromosome
+                for (let rec of block.records) {
+
+                    // transpose?
                     if (x1 === undefined || (rec.bin1 >= x1 && rec.bin1 <= x2 && rec.bin2 >= y1 && rec.bin2 <= y2)) {
-
                         if (isNorm) {
                             const x = rec.bin1
                             const y = rec.bin2
@@ -96,25 +106,20 @@ class Straw {
                             contactRecords.push(rec);
                         }
                     }
-                });
-            });
-
-            return contactRecords;
-        } catch (error) {
-            console.error(error)
+                }
+            }
         }
 
+        return contactRecords;
     }
 
     async getNormalizationOptions() {
-        // Normalization types are computed as a side effect of loading the index.  A bit
-        // ugly but alternatives are worse.
-        await this.hicFile.getNormVectorIndex()
-        return this.hicFile.normalizationTypes;
+        return this.hicFile.getNormalizationOptions()
     }
 
     async getNVI() {
-        await this.hicFile.getNormVectorIndex()
+        await
+            this.hicFile.getNormVectorIndex()
         return this.hicFile.config.nvi;
     }
 
