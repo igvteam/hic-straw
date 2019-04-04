b215dc14afe7ddfc8e2ae702d0ff3ce5906a448d
Jim Robinson <jrobinso@broadinstitute.org>
2019-04-03 17:10:55 -0700

    Add option for url api keys

--- src/io/remoteFile.js
+++ src/io/remoteFile.js
@@ -25,15 +25,16 @@ class RemoteFile {
                 headers['Authorization'] = `Bearer ${token}`
             }
             const isSafari = navigator.vendor.indexOf("Apple") == 0 && /\sSafari\//.test(navigator.userAgent);
-
             const isChrome = navigator.userAgent.indexOf('Chrome') > -1
             const isAmazonV4Signed = this.url.indexOf("X-Amz-Signature") > -1
 
             if (isChrome && !isAmazonV4Signed) {
-                // Hack to prevent caching for byte-ranges. Attempt to fix net:err-cache errors in Chrome
-                url += this.url.includes("?") ? "&" : "?";
-                url += "someRandomSeed=" + Math.random().toString(36);
+                url = addParameter(url, "randomSeed", Math.random().toString(36))
             }
+        }
+
+        if(this.config.apiKey) {
+            url = addParameter(url, "key", this.config.apiKey)
         }
 
         const response = await fetch(url, {
@@ -84,38 +85,10 @@ function mapUrl(url) {
     }
 }
 
-function driveDownloadURL(link) {
-    var i1, i2, id;
-    // Return a google drive download url for the sharable link
-    //https://drive.google.com/open?id=0B-lleX9c2pZFbDJ4VVRxakJzVGM
-    //https://drive.google.com/file/d/1_FC4kCeO8E3V4dJ1yIW7A0sn1yURKIX-/view?usp=sharing
 
-    var id = getGoogleDriveFileID(link);
-
-    return id ? "https://www.googleapis.com/drive/v3/files/" + id + "?alt=media&supportsTeamDrives=true" : link;
-}
-
-function getGoogleDriveFileID(link) {
-
-    //https://drive.google.com/file/d/1_FC4kCeO8E3V4dJ1yIW7A0sn1yURKIX-/view?usp=sharing
-    var i1, i2;
-
-    if (link.includes("/open?id=")) {
-        i1 = link.indexOf("/open?id=") + 9;
-        i2 = link.indexOf("&");
-        if (i1 > 0 && i2 > i1) {
-            return link.substring(i1, i2)
-        }
-        else if (i1 > 0) {
-            return link.substring(i1);
-        }
-
-    }
-    else if (link.includes("/file/d/")) {
-        i1 = link.indexOf("/file/d/") + 8;
-        i2 = link.lastIndexOf("/");
-        return link.substring(i1, i2);
-    }
+function addParameter(url, name, value) {
+    const  paramSeparator = url.includes("?") ? "&" : "?";
+    return url + paramSeparator + name + "=" + value;
 }
 
 
