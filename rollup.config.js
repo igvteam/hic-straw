d4ca1b4e9b782274f18457fb0c44c77dd77d3875
Jim Robinson <jrobinso@broadinstitute.org>
2019-09-17 00:07:31 -0700

    update node version used by travis

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-const strip = require('rollup-plugin-strip');
-const babel = require('rollup-plugin-babel');
+import strip from 'rollup-plugin-strip';
+import babel from 'rollup-plugin-babel';
 
 
 export default [
