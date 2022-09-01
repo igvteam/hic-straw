45bf48ffe48a95186ba330e5a23308230d5a28d1
jrobinso <933148+jrobinso@users.noreply.github.com>
2022-09-01 10:30:22 -0700

    update dev dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import strip from 'rollup-plugin-strip';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import strip from '@rollup/plugin-strip';
 import {terser} from "rollup-plugin-terser"
 
 
